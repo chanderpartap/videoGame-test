92f2e102c507f377588e484674b7dc8dd9fd6405
Chander Partap Singh <partap.pathania@gmail.com>
2021-04-15 19:44:12 +0530

    Added Player Run and Jump Script with more floors

--- Jumpin Bunny/Assets/Scripts/PlayerController.cs
+++ Jumpin Bunny/Assets/Scripts/PlayerController.cs
@@ -7,27 +7,49 @@ public class PlayerController : MonoBehaviour
     
     private Rigidbody2D rigidBody;
     public float thrust = 10.0f;
+    public LayerMask groundLayerMask;
+    public Animator animator;
+    public float runSpeed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator.SetBool("isAlive", true);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetMouseButtonDown(0)
+        bool isOnGround = IsOnGround();
+        print("is Ground = " + isOnGround);
+        animator.SetBool("isGrounded", isOnGround);
+        if ((Input.GetMouseButtonDown(0)
             || Input.GetKeyDown(KeyCode.Space)
-            || Input.GetKeyDown(KeyCode.W))
+            || Input.GetKeyDown(KeyCode.W)) && isOnGround)
         {
-            print("Left button or space or w pressed!");
+            //print("Left button or space or w pressed!");
             jump();
         }
 
+        /*if(Input.GetKeyDown(KeyCode.D))  
+        {
+            rigidBody.AddForce(new Vector2(runSpeed, 0), ForceMode2D.Impulse);
+            //transform.position = new Vector2(transform.position.x + runSpeed * Time.deltaTime, rigidBody.position.y);
+        }*/
+        if(rigidBody.velocity.x < runSpeed)
+        {
+            rigidBody.velocity = new Vector2(runSpeed, rigidBody.velocity.y);
+        }
+       
     }
 
     void jump()
     {
         rigidBody.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
+    }
+    bool IsOnGround()
+    {
+        //print("Mask = " + groundLayerMask);
+        return Physics2D.Raycast(this.transform.position, Vector2.down, 1.0f, groundLayerMask.value);
     }
 }
