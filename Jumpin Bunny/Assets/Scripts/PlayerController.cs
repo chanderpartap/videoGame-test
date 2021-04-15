7b517af7b0bc3448172e502d447e0a6307da0399
Chander Partap Singh <partap.pathania@gmail.com>
2021-04-15 14:11:43 +0530

    Added Force to Jump

--- Jumpin Bunny/Assets/Scripts/PlayerController.cs
+++ Jumpin Bunny/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rigidBody;
+    public float thrust = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)
+            || Input.GetKeyDown(KeyCode.Space)
+            || Input.GetKeyDown(KeyCode.W))
+        {
+            print("Left button or space or w pressed!");
+            jump();
+        }
+
+    }
+
+    void jump()
+    {
+        rigidBody.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
     }
 }
