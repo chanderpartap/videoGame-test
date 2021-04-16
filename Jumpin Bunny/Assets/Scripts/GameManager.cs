fb714b38b6181db3e03380affd0b22895773261e
Chander Partap Singh <partap.pathania@gmail.com>
2021-04-16 10:34:49 +0530

    Game manger Added with Game States

---
+++ Jumpin Bunny/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* <summary>
+/// 1> Main Menu
+/// 2> In Game
+/// 3> Game Over
+/// 4> Pause
+/ </summary>*/
+
+enum GameState
+{
+    Menu,
+    InGame,
+    GameOver
+}
+public class GameManager : MonoBehaviour
+{
+    GameState currentGameState = GameState.Menu; //Default value
+    // Start and Update not required
+    //To Start Game
+    public void StartGame()
+    {
+        ChangeGameState(GameState.InGame);
+    }
+    //To End Game
+    public void GameOver()
+    {
+        ChangeGameState(GameState.GameOver);
+    }
+    //When player wants to Quit the game and go back to main menu
+    public void BackToMainMenu()
+    {
+        ChangeGameState(GameState.Menu);
+    }
+    //Method to help change states
+    //All logic goes here (mostly)
+    void ChangeGameState(GameState newGameState)
+    {
+        if(newGameState == GameState.Menu)
+        {
+            //Load Main Menu
+
+        } else if(newGameState == GameState.InGame)
+        {
+            //Load the Game
+
+        } else if(newGameState == GameState.GameOver)
+        {
+            //Load Scores or End game
+
+        }
+        else
+        {
+            newGameState = GameState.Menu;
+        }
+        //Construct Swith in Scripting
+        /*
+         * switch(newGameState)
+         * {
+         * case GameState.Menu
+         *Load menu
+         *break;
+         * }
+         */
+        currentGameState = newGameState;
+
+    }
+    
+}
