c31d0344a6f86b5e7f1984a18bf0c36fc895f094
Chander Partap Singh <partap.pathania@gmail.com>
2021-04-16 12:22:40 +0530

    The Level Generator

---
+++ Jumpin Bunny/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public List<LevelBlock> legoBlocks = new List<LevelBlock>(); // Sample blocks from where to begin
+    List<LevelBlock> currentBlocks = new List<LevelBlock>(); // Blocks added to the game already
+    public Transform initialPoint;
+
+    private static LevelGenerator _sharedInstance;
+    public static LevelGenerator sharedInstance //Another way of defining a getter method
+    {
+        get
+        {
+            return _sharedInstance;
+        }
+    }
+
+    private void Awake()
+    {
+        _sharedInstance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddNewBlock()
+    {
+
+    }
+}
